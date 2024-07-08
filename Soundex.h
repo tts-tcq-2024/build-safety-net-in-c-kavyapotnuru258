1b72099a58484606a8b1607f3343db33a8dc116d
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-08 16:49:54 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,54 +1,50 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
-
+#include <stdio.h>
+ 
 #define GET_SOUNDEX_CODE(c) (\
-    (c== 'B'|| c== 'F'||c=='P' ||c=='V')? '1':\
-    (c== 'C'|| c== 'G'||c=='J' ||c=='K'||c== 'Q'|| c== 'S'||c=='X' ||c=='Z')? '2' :\
-    (c== 'D'|| c== 'T') ? '3':\
-    (c== 'L') ? '4' : \
-    (c== 'M'|| c== 'N') ? '5' :\
-     (c== 'R') ?'6' :\
-     '0')
-
-
+    (c == 'B' || c == 'F' || c == 'P' || c == 'V') ? '1' : \
+    (c == 'C' || c == 'G' || c == 'J' || c == 'K' || c == 'Q' || c == 'S' || c == 'X' || c == 'Z') ? '2' : \
+    (c == 'D' || c == 'T') ? '3' : \
+    (c == 'L') ? '4' : \
+    (c == 'M' || c == 'N') ? '5' : \
+    (c == 'R') ? '6' : \
+    '0')
+ 
 char getSoundexCode(char c) {
     c = toupper(c);
-    return  GET_SOUNDEX_CODE(c);
+    return GET_SOUNDEX_CODE(c);
 }
-void addingcode( int *sIndex, char code,char *soundex)
-{
-   if (code != '0' && code != soundex[*sIndex - 1]) {
-            soundex[*sIndex] = code;
-            *(sIndex)++;
-   }
-}
-void zeropadding(int *sIndex,char *soundex)
-{
- while (*sIndex < 4) {
-        soundex[*sIndex] = '0';
-          *(sIndex)++;
+ 
+void addingcode(int *sIndex, char code, char *soundex) {
+    if (code != '0' && code != soundex[*sIndex - 1]) {
+        soundex[*sIndex] = code;
+        (*sIndex)++;
     }
-
+}
+ 
+void zeropadding(int *sIndex, char *soundex) {
+    while (*sIndex < 4) {
+        soundex[*sIndex] = '0';
+        (*sIndex)++;
+    }
     soundex[4] = '\0';
 }
-
-
-
+ 
 void generateSoundex(const char *name, char *soundex) {
     int len = strlen(name);
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
-
+ 
     for (int i = 1; i < len && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        addingcode( &sIndex,code,soundex);
+        addingcode(&sIndex, code, soundex);
     }
-
-    zeropadding(&sIndex,soundex);
+ 
+    zeropadding(&sIndex, soundex);
 }
 
 #endif // SOUNDEX_H
