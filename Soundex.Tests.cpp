3617bd7d2d67356025af0b02a5ea622ab2b19826
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-08 16:52:09 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,6 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("AXD" ,soundex);
-  ASSERT_STREQ(soundex,"A230");
+  generateSoundex("AX" ,soundex);
+  ASSERT_STREQ(soundex,"A200");
 }
